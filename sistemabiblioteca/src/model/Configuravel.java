a4b571eb3f04c8af45e4648cef33983cbffa501f
Carlos Antonio <carlosantonioob@gmail.com>
2025-05-01 08:34:24 -0300

    Foi adicionada a classe interface (Configuravel)

---
+++ sistemabiblioteca/src/model/Configuravel.java
@@ -0,0 +1,9 @@
+package model;
+
+public interface Configuravel {
+
+    
+    public int definirTempoPadraoEmprestimo(int dias);
+    public int definirMaximoEmprestimo(int quantidade);
+    public double definirValorMulta(double valorMulta);
+}
